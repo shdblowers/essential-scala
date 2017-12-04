41b6bd9c7ec974370b4b5c60d494595083e90a57
Steven Blowers <shdblowers@gmail.com>
2017-12-04 17:43:22 +0000

    re-implement Director and Film with case class

--- src/main/scala/directors/Director.scala
+++ src/main/scala/directors/Director.scala
@@ -1,21 +1,11 @@
 package directors
 
-class Director(val firstName: String, val lastName: String, val yearOfBirth: Int) {
+case class Director(firstName: String, lastName: String, yearOfBirth: Int) {
 
   def name: String = s"$firstName $lastName"
-
-  def copy(firstName: String = this.firstName,
-           lastName: String = this.lastName,
-           yearOfBirth: Int = this.yearOfBirth): Director = {
-    new Director(firstName, lastName, yearOfBirth)
-  }
 }
 
 object Director {
-  def apply(firstName: String, lastName: String, yearOfBirth: Int): Director = {
-    new Director(firstName, lastName, yearOfBirth)
-  }
-
   def older(directorA: Director, directorB: Director): Director = {
     if (directorA.yearOfBirth < directorB.yearOfBirth) directorA else directorB
   }
